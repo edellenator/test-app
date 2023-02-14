6579ef53a0c98595071de9d6d0d5b569a7ba3b11
Erik Dell <edellenator@gmail.com>
2023-02-14 09:12:35 -0800

    twitter account

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
 
+  get "auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
+  resources :twitter_accounts
+
   root to: "main#index"
   # Defines the root path route ("/")
   # root "articles#index"
